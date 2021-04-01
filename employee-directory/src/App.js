c8e826b01941d5b1bb6ec2bc86b90046e494a705
EJostad <eric.jostad@outlook.com>
2021-03-31 23:00:23 -0700

    Installed react-bootstrap package. Created Title, SearchBar, and EmployeeTable components within App.js file.

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,9 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { Form, Table } from 'react-bootstrap';
+
+const Title = () => {
+  <h1 style={{ width: '100%', textAlign: 'center' }}>Employee Directory</h1>
+};
+
+const SearchBar = () => {
+  return <Form.Control type="text" placeholder="Normal text" />
+};
+
+const EmployeeTable = () => {
+  return (
+    <Table striped bordered hover>
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Username</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td colSpan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</Table>
+  );
+};
+
 
 function App() {
   return (
-    <h1>Employee Directory</h1>
+    <>
+      <Title/>
+      <SearchBar/>
+      <EmployeeTable/>
+    </>
   );
 }
 
