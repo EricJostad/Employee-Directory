07052fb91d061f41803d1927c160b5407b8e0a57
EJostad <eric.jostad@outlook.com>
2021-04-03 23:44:03 -0700

    Added filter funtionality to search bar.

--- employee-directory/src/components/EmployeeTable.js
+++ employee-directory/src/components/EmployeeTable.js
@@ -5,12 +5,22 @@ const EmployeeTable = ({ searchTerm }) => {
   const [employees, setEmployees] = useState([]);
 
   useEffect(() => {
-    fetch("https://randomuser.me/api/?results=100")
+    fetch("https://randomuser.me/api/?results=500&nat=us")
       .then((res) => res.json())
       .then((res) => {
         setEmployees(res.results);
       });
   }, []);
+
+  const searchHas = (str, match) =>
+    str.toLowerCase().indexOf(match.toLowerCase()) !== -1;
+
+  const handleSearchField = ({ name, location, dob }) =>
+    !searchTerm ||
+    searchHas(name.first, searchTerm) ||
+    searchHas(name.last, searchTerm)  ||
+    searchHas(location.city, searchTerm);
+    // searchHas(dob.date, searchTerm)
 
   return (
     <Table striped bordered hover>
@@ -20,26 +30,28 @@ const EmployeeTable = ({ searchTerm }) => {
           <th>First Name</th>
           <th>Last Name</th>
           <th>DOB</th>
+          <th>Location</th>
           <th>Email</th>
         </tr>
       </thead>
       <tbody>
         {employees
-          .filter((e) => !searchTerm || e.name.first.indexOf(searchTerm) !== -1)
-          .map(({ dob, name, email }, i) => (
-            <EmployeeRow dob={dob} name={name} email={email} i={i} />
+          .filter(handleSearchField)
+          .map(({ dob, name, location, email }, i) => (
+            <EmployeeRow dob={dob} name={name} location={location} email={email} i={i} />
           ))}
       </tbody>
     </Table>
   );
 };
 
-const EmployeeRow = ({ dob, name, email, i }) => (
+const EmployeeRow = ({ dob, name, location, email, i }) => (
   <tr key={i}>
     <td>{i}</td>
     <td>{name.first}</td>
     <td>{name.last}</td>
     <td>{dob.date}</td>
+    <td>{location.city}, {location.state}</td>
     <td>{email}</td>
   </tr>
 );
--- employee-directory/src/components/SearchBar.js
+++ employee-directory/src/components/SearchBar.js
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Form } from "react-bootstrap";
 
 const SearchBar = ({ searchTerm, setSearchTerm }) => {
     return (
       <Form.Control
         type="text"
-        placeholder="Normal text"
+        placeholder="Enter in name, date of birth, age, location, or employee number to filter."
         value={searchTerm}
         onChange={({ target }) => setSearchTerm(target.value)}
       />
