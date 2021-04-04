7eb49d863b9c7b71d266a1bd938322f94f1dd530
EJostad <eric.jostad@outlook.com>
2021-04-03 23:46:15 -0700

    Updated placeholder te and removed filter fnctionality that is not working yet.

--- employee-directory/src/components/SearchBar.js
+++ employee-directory/src/components/SearchBar.js
@@ -5,7 +5,7 @@ const SearchBar = ({ searchTerm, setSearchTerm }) => {
     return (
       <Form.Control
         type="text"
-        placeholder="Enter in name, date of birth, age, location, or employee number to filter."
+        placeholder="Enter in name or office location to filter."
         value={searchTerm}
         onChange={({ target }) => setSearchTerm(target.value)}
       />
